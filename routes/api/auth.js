a9aaaa8d1075fc3c86088bfe55bc2fb80784a133
Tefa600 <67193168+Tefa600@users.noreply.github.com>
2021-12-24 11:40:22 +0200

    auth

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -8,7 +8,7 @@ const User = require("../../models/User");
 //@acess        public
 router.get("/", auth, async (req, res) => {
   try {
-    const user = User.findById(req.user.id).select("-password");
+    const user = await User.findById(req.user.id).select("-password");
     res.json(user);
   } catch (err) {
     console.error(err.message);
